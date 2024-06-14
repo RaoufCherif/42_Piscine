04a05e31c9e2a6f06e40157addc6f38c376826f6
codephenix4 <yahia19raouf@gmail.com>
2024-06-14 10:10:21 +0200

    c01/ex00

--- c01/ex00/ft_ft.c
+++ c01/ex00/ft_ft.c
@@ -1,12 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ec                                          :+:      :+:    :+:   */
+/*   ft_ft.c ex00                                        :+      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: YC Raouf <yahia19raouf@gmail.com>          +#+  +:+       +#+        */
+/*   By: YC Raouf <raouf19raouf@gmail.com           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/27 12:07:37 by nshahid           #+#    #+#             */
-/*   Updated: 2023/05/27 12:07:37 by nshahid          ###   ########.fr       */
+/*   Created: 2024/03/30 17:21:42 by cmassol           #+#    #+#             */
+/*   Updated: 2024/03/30 21:55:40 by cmassol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
+#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+int	main(void)
+{
+	int	a;
+
+	a = 1;
+	printf (" %i\n ", a);
+	printf (" %p\n ", &a);
+	ft_ft (&a);
+	printf ("%i\n", a);
+	printf ("%p\n", &a);
+	return (0);
+}
